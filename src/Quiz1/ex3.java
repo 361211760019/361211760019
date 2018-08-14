041eded33a3730fbc39a770bf548d002c238b8e5
chadarat <srihamak2540@gmail.com>
2018-08-14 11:55:04 +0700

    Quiz1

---
+++ src/Quiz1/ex3.java
@@ -0,0 +1,28 @@
+package Quiz1;
+
+import java.util.Scanner;
+
+public class ex3 {
+    public static void main(String[] args) {
+
+
+        int ;
+        System.out.println("เลือกการค่าห้องพักโรงแรม ?");
+        System.out.println(" Room Type 1 ");
+        System.out.println(" Room Type 2 ");
+        System.out.println(" Room Type 3 ");
+        System.out.println(" Room Type 4 ");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("เลือกประเภทห้องพัก(1-4):");
+        select = input.nextInt();
+
+        System.out.println("จำนวนวันที่เข้าพัก: ");
+
+        System.out.println("ค่าห้องที่ต้องจ่าย: ");
+
+
+
+
+    }
+}
