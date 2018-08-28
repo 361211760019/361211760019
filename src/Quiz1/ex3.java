8e81526cb704d9b0de2f12aaabd1a681d9632fb0
chadarat <srihamak2540@gmail.com>
2018-08-28 11:00:48 +0700

    lab4: Methods Overloading

--- src/Quiz1/ex3.java
+++ src/Quiz1/ex3.java
@@ -6,20 +6,7 @@ public class ex3 {
     public static void main(String[] args) {
 
 
-        int ;
-        System.out.println("เลือกการค่าห้องพักโรงแรม ?");
-        System.out.println(" Room Type 1 ");
-        System.out.println(" Room Type 2 ");
-        System.out.println(" Room Type 3 ");
-        System.out.println(" Room Type 4 ");
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("เลือกประเภทห้องพัก(1-4):");
-        select = input.nextInt();
-
-        System.out.println("จำนวนวันที่เข้าพัก: ");
-
-        System.out.println("ค่าห้องที่ต้องจ่าย: ");
 
 
 
