1d3ddb13a4e2549e9fc7a9d524bc2fd6be08d74c
chadarat <srihamak2540@gmail.com>
2018-09-18 11:16:14 +0700

    lab5:StringBuffer and StringBuilder

---
+++ src/lab5/ex5.java
@@ -0,0 +1,21 @@
+package lab5;
+
+public class ex5 {
+
+    public static void main(String[] args) {
+        String msg1 = "Hello";
+        String msg2 = "Hello";
+        String msg3 = "hello";
+
+        if (msg1.equals(msg2)) System.out.println(true);
+        else System.out.println(false);
+        if (msg1.equals(msg3)) System.out.println(true);
+        else System.out.println(false);
+
+        if (msg1.compareTo(msg3)==0)
+            System.out.println("Two string is equal.");
+        else if (msg1.compareTo(msg3)>=1)
+            System.out.println("msg1 more then msg3");
+        else System.out.println("msg1 less then msg3");
+    }
+}
